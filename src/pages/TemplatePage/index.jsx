7f2663a5747a69d5c678a71141ba6a4ad5dfd022
nikita.ma <nikita.ma@fora-soft.com>
2022-12-09 19:09:09 +0300

    display created entities

--- src/pages/TemplatePage/index.jsx
+++ src/pages/TemplatePage/index.jsx
@@ -1,37 +1,74 @@
-import React from 'react';
+import React, { useMemo, useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import About from "../../components/About";
-import BrandBg from "../../components/BrandBg";
-import ImgBlock from "../../components/ImgBlock";
+import About from '../../components/About';
+import BrandBg from '../../components/BrandBg';
+import ImgBlock from '../../components/ImgBlock';
 import Preloader from '../../common/Preloader';
 
-import {getPage} from '../../functions/data';
+import { REQUEST_TYPE, useRequest } from '../../hooks/useRequest';
+import { NotificationContext } from '../../context/NotificationContext';
+import NOTIFICATION_TYPES from '../../const/notifications/NOTIFICATION_TYPES';
+import { useSelector } from 'react-redux';
 
 const TemplatePage = () => {
-    const [pageData, setPageData] = React.useState("");
-    const [loading, setLoading] = React.useState(true);
-    const {url} = useParams();
+    const accounts = useSelector(state => state.accounts);
 
-    React.useEffect(() => {
+    const [pageInfo, setPageInfo] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const {
+        actions: { addNotification },
+    } = useContext(NotificationContext);
+
+    const { url } = useParams();
+
+    const { state: getPageInfoRS, request: onGerPageInfo } = useRequest({
+        requestType: REQUEST_TYPE.DATA,
+    });
+
+    const pageAccaunts = useMemo(() => {
+        if (!pageInfo || !accounts.accounts) {
+            return [];
+        }
+
+        return accounts.accounts.filter(a => a.page === pageInfo.id);
+    }, [accounts.accounts, pageInfo]);
+
+    useEffect(() => {
         setLoading(true);
-        const pageInfo = getPage(url);
-        pageInfo.then(data => setPageData(data.data)).finally(() => setLoading(false));
+        onGerPageInfo({
+            url: `page/${url}/`,
+        });
     }, [url]);
 
-    if(loading){
-        return <Preloader />
+    useEffect(() => {
+        setLoading(false);
+        if (getPageInfoRS.result && getPageInfoRS.result.data) {
+            setPageInfo(getPageInfoRS.result.data);
+        }
+
+        if (getPageInfoRS.error) {
+            addNotification({
+                type: NOTIFICATION_TYPES.ERROR,
+                text: getPageInfoRS.error,
+            });
+        }
+    }, [getPageInfoRS]);
+
+    if (loading || getPageInfoRS.isProcessing || !pageInfo) {
+        return <Preloader />;
     }
 
-    return(
+    return (
         <>
-            <BrandBg src={pageData.banner} />
+            <BrandBg src={pageInfo.banner} />
 
-            <About title={pageData.title_1} text={pageData.description} />
+            <About title={pageInfo.title_1} text={pageInfo.description} />
 
-            <ImgBlock title={pageData.title_2} data={[]} />
+            <ImgBlock title={pageInfo.title_2} data={pageAccaunts} />
         </>
-    )
-}
+    );
+};
 
 export default TemplatePage;
